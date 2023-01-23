d2969790eb5a22ee0bf4cf595e756c327b15b5f1
Euphoria13 <kevineyang271@gmail.com>
2023-01-22 19:59:07 -0600

    added in spawning towards center of the map

--- src/gopher3/robots/Headquarters.java
+++ src/gopher3/robots/Headquarters.java
@@ -9,11 +9,58 @@ import java.util.Stack;
 public class Headquarters extends Robot {
 
     private int[] implicitStack;
-
+    public Direction[] DirPref=new Direction[8];
 
     public Headquarters(RobotController rc) throws Exception {
         super(rc);
-
+        int setup=0;
+        if(rc.getMapHeight()/2>rc.getLocation().y){
+            setup++;
+        }
+        if(rc.getMapWidth()/2>rc.getLocation().x){
+            setup+=2;
+        }
+        switch (setup){
+            case 0:
+                DirPref[0]= Direction.SOUTHWEST;
+                DirPref[1]= Direction.WEST;
+                DirPref[2]= Direction.SOUTH;
+                DirPref[3]= Direction.NORTHWEST;
+                DirPref[4]= Direction.SOUTHEAST;
+                DirPref[5]= Direction.NORTH;
+                DirPref[6]= Direction.EAST;
+                DirPref[7]= Direction.NORTHEAST;
+                break;
+            case 1:
+                DirPref[0]= Direction.NORTHWEST;
+                DirPref[1]= Direction.WEST;
+                DirPref[2]= Direction.NORTH;
+                DirPref[3]= Direction.SOUTHWEST;
+                DirPref[4]= Direction.NORTHEAST;
+                DirPref[5]= Direction.SOUTH;
+                DirPref[6]= Direction.EAST;
+                DirPref[7]= Direction.SOUTHEAST;
+                break;
+            case 2:
+                DirPref[0]= Direction.SOUTHEAST;
+                DirPref[1]= Direction.EAST;
+                DirPref[2]= Direction.SOUTH;
+                DirPref[3]= Direction.NORTHEAST;
+                DirPref[4]= Direction.SOUTHWEST;
+                DirPref[5]= Direction.NORTH;
+                DirPref[6]= Direction.WEST;
+                DirPref[7]= Direction.NORTHWEST;
+                break;
+            default:
+                DirPref[0]= Direction.NORTHEAST;
+                DirPref[1]= Direction.EAST;
+                DirPref[2]= Direction.NORTH;
+                DirPref[3]= Direction.SOUTHEAST;
+                DirPref[4]= Direction.NORTHWEST;
+                DirPref[5]= Direction.SOUTH;
+                DirPref[6]= Direction.WEST;
+                DirPref[7]= Direction.SOUTHWEST;
+        }
         Stack<MapLocation> s = new Stack<>();
         implicitStack = new int[10];
         int implicitStackCurrent = 0;
@@ -33,7 +80,7 @@ public class Headquarters extends Robot {
 
     private MapLocation getOpenSpawnLocation() throws GameActionException {
         MapLocation spawnLoc = rc.getLocation();
-        for (Direction dir : pathing.DIRECTIONS) {
+        for (Direction dir : DirPref) {
             MapLocation newLoc = spawnLoc.add(dir);
             if (rc.onTheMap(newLoc) && !rc.isLocationOccupied(newLoc) && rc.sensePassability(newLoc)) {
                 return newLoc;
@@ -81,10 +128,6 @@ public class Headquarters extends Robot {
             rc.buildRobot(RobotType.CARRIER, freeLoc);
         } else if (freeLoc != null && rc.canBuildRobot(RobotType.LAUNCHER, freeLoc)) {
             rc.buildRobot(RobotType.LAUNCHER, freeLoc);
-        }
-
-        if (rc.getRoundNum() > 100) {
-            rc.resign();
         }
 
         endTurn();
--- src/gopher4/robots/Headquarters.java
+++ src/gopher4/robots/Headquarters.java
@@ -6,14 +6,62 @@ import java.util.Stack;
 
 
 public class Headquarters extends Robot {
-
+    public Direction[] DirPref=new Direction[8];
     public Headquarters(RobotController rc) throws Exception {
         super(rc);
+        int setup=0;
+        if(rc.getMapHeight()/2>rc.getLocation().y){
+            setup++;
+        }
+        if(rc.getMapWidth()/2>rc.getLocation().x){
+            setup+=2;
+        }
+        switch (setup){
+            case 0:
+                DirPref[0]= Direction.SOUTHWEST;
+                DirPref[1]= Direction.WEST;
+                DirPref[2]= Direction.SOUTH;
+                DirPref[3]= Direction.NORTHWEST;
+                DirPref[4]= Direction.SOUTHEAST;
+                DirPref[5]= Direction.NORTH;
+                DirPref[6]= Direction.EAST;
+                DirPref[7]= Direction.NORTHEAST;
+                break;
+            case 1:
+                DirPref[0]= Direction.NORTHWEST;
+                DirPref[1]= Direction.WEST;
+                DirPref[2]= Direction.NORTH;
+                DirPref[3]= Direction.SOUTHWEST;
+                DirPref[4]= Direction.NORTHEAST;
+                DirPref[5]= Direction.SOUTH;
+                DirPref[6]= Direction.EAST;
+                DirPref[7]= Direction.SOUTHEAST;
+                break;
+            case 2:
+                DirPref[0]= Direction.SOUTHEAST;
+                DirPref[1]= Direction.EAST;
+                DirPref[2]= Direction.SOUTH;
+                DirPref[3]= Direction.NORTHEAST;
+                DirPref[4]= Direction.SOUTHWEST;
+                DirPref[5]= Direction.NORTH;
+                DirPref[6]= Direction.WEST;
+                DirPref[7]= Direction.NORTHWEST;
+                break;
+            default:
+                DirPref[0]= Direction.NORTHEAST;
+                DirPref[1]= Direction.EAST;
+                DirPref[2]= Direction.NORTH;
+                DirPref[3]= Direction.SOUTHEAST;
+                DirPref[4]= Direction.NORTHWEST;
+                DirPref[5]= Direction.SOUTH;
+                DirPref[6]= Direction.WEST;
+                DirPref[7]= Direction.SOUTHWEST;
+        }
     }
 
     private MapLocation getOpenSpawnLocation() throws GameActionException {
         MapLocation spawnLoc = rc.getLocation();
-        for (Direction dir : pathing.DIRECTIONS) {
+        for (Direction dir : DirPref) {
             MapLocation newLoc = spawnLoc.add(dir);
             if (rc.onTheMap(newLoc) && !rc.isLocationOccupied(newLoc) && rc.sensePassability(newLoc)) {
                 return newLoc;
@@ -25,6 +73,7 @@ public class Headquarters extends Robot {
     public void run() throws GameActionException, IllegalAccessException {
 
         super.run();
+
 
         MapLocation freeLoc = getOpenSpawnLocation();
         if (freeLoc != null && rc.canBuildRobot(RobotType.CARRIER, freeLoc)) {
