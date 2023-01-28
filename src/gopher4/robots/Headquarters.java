70c7ec76ad1060c65a9d53690bf96025b99f2e9b
Euphoria13 <kevineyang271@gmail.com>
2023-01-28 00:53:45 -0600

    Launcher missing Targeting

--- src/gopher4/robots/Headquarters.java
+++ src/gopher4/robots/Headquarters.java
@@ -13,9 +13,12 @@ public class Headquarters extends Robot {
             {-2,-1}, {-1,-1}, {0,-1}, {1,-1}, {2,-1},
             {0,-3}
     };
+    int Launchers;
+    int symmetryType;
     public Headquarters(RobotController rc) throws Exception {
         super(rc);
-
+        Launchers=0;
+        symmetryType=0;
         comms.reportSelfHQLocation();
     }
 
@@ -24,7 +27,7 @@ public class Headquarters extends Robot {
         MapLocation minLoc=null;
         int minDist=100;
         int heuro=0;
-        MapLocation thePoint= new MapLocation (rc.getMapHeight()/2, rc.getMapWidth()/2);
+        MapLocation thePoint= new MapLocation (rc.getMapWidth()/2, rc.getMapHeight()/2);
         if(getNearestKnownWell(null)!=null) {
             thePoint= getNearestKnownWell(null);
         }
@@ -42,12 +45,12 @@ public class Headquarters extends Robot {
     public void run() throws GameActionException, IllegalAccessException {
 
         super.run();
-
-
+        symmetryType=rc.readSharedArray(63)/16384;
         MapLocation freeLoc = getOpenSpawnLocation();
         if (freeLoc != null && rc.canBuildRobot(RobotType.CARRIER, freeLoc)) {
             rc.buildRobot(RobotType.CARRIER, freeLoc);
         } else if (freeLoc != null && rc.canBuildRobot(RobotType.LAUNCHER, freeLoc)) {
+            rc.writeSharedArray(63,Launchers/3+symmetryType*16384);
             rc.buildRobot(RobotType.LAUNCHER, freeLoc);
         }
 
